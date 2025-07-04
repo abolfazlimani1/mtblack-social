a8a5defc4bca1444c8ea8cad3d637b9a0bc1731a
abolfazlimani1 <mabolfazlimani1@gmail.com>
2025-07-04 14:38:34 +0330

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCcQRSbI18O4xkvcrOApjtAj4EbR1J4dDo",
+    authDomain: "mtblack-630c5.firebaseapp.com",
+    projectId: "mtblack-630c5",
+    storageBucket: "mtblack-630c5.appspot.com",
+    messagingSenderId: "689920201467",
+    appId: "1:689920201467:web:a13f864568e508f495565e",
+    measurementId: "G-YWHP0Y6KMY"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
